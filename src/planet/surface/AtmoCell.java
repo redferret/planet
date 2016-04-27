38a340da2537f85412dd3323c0f39a46557cebe3
Richard DeSilvey <rdesilvey@gmail.com>
2016-04-26 23:53:09 -0600

    Added Class comments

--- src/planet/surface/AtmoCell.java
+++ src/planet/surface/AtmoCell.java
@@ -4,7 +4,8 @@ package planet.surface;
 
 
 /**
- *
+ * The atmosphere is represented by the AtmoCell. Climate and weather are 
+ * controlled by this class. 
  * @author Richard DeSilvey
  */
 public class AtmoCell extends BioCell{
--- src/planet/surface/BioCell.java
+++ src/planet/surface/BioCell.java
@@ -3,7 +3,8 @@
 package planet.surface;
 
 /**
- *
+ * The Biosphere is represented by this cell. The biosphere includes all life
+ * forms and their impact on the environment.
  * @author Richard DeSilvey
  */
 public class BioCell extends HydroCell {
