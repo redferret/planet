231f1a51e45242afb0c63071e6d561d46667acc6
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-01 21:23:40 -0600

    Built up class

--- src/planet/mantel/Mantel.java
+++ src/planet/mantel/Mantel.java
@@ -2,8 +2,10 @@
 
 package planet.mantel;
 
+import java.awt.Color;
 import java.util.List;
 import planet.generics.Cell;
+import planet.util.Tools;
 
 /**
  *
@@ -11,12 +13,26 @@ import planet.generics.Cell;
  */
 public class Mantel extends Cell {
 
+    private float temperature;
+    private static Integer[][] heatMap;
+    
+    static {
+        Color[] colors = {new Color(95, 0, 15), new Color(255, 45, 45), new Color(250, 250, 0)};
+        heatMap = Tools.constructSamples(colors, 100);
+    }
+    
     public Mantel(int x, int y) {
         super(x, y);
+        
+        temperature = 0;
     }
 
     @Override
     public List<Integer[]> render(List<Integer[]> settings) {
+        
+        int index = (int) (temperature / 10);
+        settings.add(heatMap[index]);
+        
         return settings;
     }
 
