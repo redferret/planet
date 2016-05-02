17dc2f58224e16b14c74a9686828a0ccc5848b7f
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-01 21:30:11 -0600

    Added methods

--- src/planet/mantel/Mantel.java
+++ src/planet/mantel/Mantel.java
@@ -8,17 +8,20 @@ import planet.generics.Cell;
 import planet.util.Tools;
 
 /**
- *
+ * The mantel is below the crust, heat and magma that build up to critical
+ * points below the crust cause volcanoes to erupt.
  * @author Richard DeSilvey
  */
 public class Mantel extends Cell {
 
+    public static float criticalTemperature;
     private float temperature;
     private static Integer[][] heatMap;
     
     static {
         Color[] colors = {new Color(95, 0, 15), new Color(255, 45, 45), new Color(250, 250, 0)};
         heatMap = Tools.constructSamples(colors, 100);
+        criticalTemperature = 1200;
     }
     
     public Mantel(int x, int y) {
@@ -27,10 +30,22 @@ public class Mantel extends Cell {
         temperature = 0;
     }
 
+    public void addHeat(float amount){
+        temperature += amount;
+    }
+    
+    public void cool(){
+        temperature--;
+    }
+    
+    public float getMantelTemperature() {
+        return temperature;
+    }
+    
     @Override
     public List<Integer[]> render(List<Integer[]> settings) {
         
-        int index = (int) (temperature / 10);
+        int index = (int) (temperature / 12);
         settings.add(heatMap[index]);
         
         return settings;
