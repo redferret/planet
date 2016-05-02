17ad4124963f0413c75936b4e80b619bea31c595
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-02 12:38:44 -0600

    Formatted

--- src/planet/mantel/Mantel.java
+++ src/planet/mantel/Mantel.java
@@ -16,9 +16,11 @@ import planet.util.Tools;
 public class Mantel extends Cell {
 
     public static float criticalTemperature;
-    private float temperature;
     private static Integer[][] heatMap;
     private static Random rand;
+    
+    private float temperature;
+    
     static {
         Color[] colors = {new Color(95, 0, 15), new Color(255, 45, 45), new Color(250, 250, 0)};
         heatMap = Tools.constructSamples(colors, 100);
