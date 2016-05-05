65bf01a65c42a2b9e51ed0da4c9a3489b2190d82
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-05 11:53:51 -0600

    Added and edited documentation

--- src/planet/gui/DisplayAdapter.java
+++ src/planet/gui/DisplayAdapter.java
@@ -3,16 +3,21 @@ package planet.gui;
 
 
 /**
- * The DisplayAdapter provides an additional draw method after the Frame is
- * finished rendering. If the Display is not drawing SurfaceMaps then this
- * adapter can be used to access drawing. This method is called after
- * SurfaceMaps are drawn (if any) and SpriteObjects currently held in the
- * engine.
+ * Connects the simulation and any display object. The method encapsulated in
+ * this interface will be called by the Surface object each time it updates.
+ * This doesn't mean that the surface is ready or not being worked on but still
+ * signals your display to refresh.
  * 
  * @author Richard DeSilvey
  */
 public interface DisplayAdapter {
     
+    /**
+     * On each update to the surface the simulation will call this method.
+     * Rendering doesn't have to always happen when this method is called,
+     * it is more of a way to flag your display when the data has finished
+     * updating on the current frame.
+     */
     public void repaint();
     
 }
