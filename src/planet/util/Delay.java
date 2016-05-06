75fcaf9c730d15cc8e9b7ed616a35ecacff75c9b
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-06 17:01:13 -0600

    Changed method names and formatted file

--- src/planet/util/Delay.java
+++ src/planet/util/Delay.java
@@ -1,83 +1,86 @@
-
 package planet.util;
 
 import java.io.Serializable;
 
 /**
- * Delay objects are used when something needs a slower update
- * than what your main thread.
- * 
+ * Delay objects are used when something needs a slower update than what your
+ * main thread.
+ *
  * @author Richard DeSilvey
  */
 public final class Delay implements Serializable {
 
     private int delayTime, maxDelay;
-    
+
     private boolean reset;
-    
+
     /**
      * Default constructor
      */
-    public Delay(){
+    public Delay() {
         delayTime = 0;
         maxDelay = 0;
         reset = true;
     }
-    
+
     /**
      * Default constructor
+     *
      * @param reset
      */
-    public Delay(boolean reset){
+    public Delay(boolean reset) {
         delayTime = 0;
         maxDelay = 0;
         this.reset = reset;
     }
-    
+
     /**
-     * Initializes this delay with the number of frames to delay at.
+     * Initializes this check with the number of frames to check at.
+     *
      * @param frames
      */
-    public Delay(int frames){
+    public Delay(int frames) {
         assert frames <= 0 : "Invalid Delay Time";
-        
+
         delayTime = 0;
         this.maxDelay = frames;
         reset = true;
     }
-    
+
     /**
-     * Initializes this delay with the number of frames to delay at.
+     * Initializes this check with the number of frames to check at.
+     *
      * @param frames
      * @param reset
      */
-    public Delay(int frames, boolean reset){
-        
+    public Delay(int frames, boolean reset) {
+
         assert frames <= 0 : "Invalid Delay Time";
-        
+
         delayTime = 0;
         this.maxDelay = frames;
         this.reset = reset;
     }
 
     /**
-     * When the delay is finished, this boolean value
-     * will determine whether or not to reset this delay.
-     * By default the reset is turned off.
+     * When the check is finished, this boolean value will determine whether or
+     * not to reset this check. By default the reset is turned off.
+     *
      * @param b
      */
-    public void allowReset(boolean b){
+    public void allowReset(boolean b) {
         reset = b;
     }
-    
+
     /**
-     * Specifies the number of frames to delay and returns 1 if the delay has been met
-     * or 0 if the delay has not yet been reached.
+     * Specifies the number of frames to check and returns 1 if the check has
+     * been met or 0 if the check has not yet been reached.
+     *
      * @param frames
-     * @return 
+     * @return
      */
-    public boolean delay(int frames){
-        
+    public boolean check(int frames) {
+
         assert frames < 0 : "Invalid Delay Time";
 
         if (++delayTime >= frames) {
@@ -94,34 +97,34 @@ public final class Delay implements Serializable {
             return false;
         }
     }
-    
+
     /**
-     * Test delay only works if you initialize your delay object
-     * with a delay value given in the constructor's parameter.
-     * This does not increment the delay.
-     * @return 1 if the delay is met, 0 if the delay is not met.
+     * Test check only works if you initialize your check object with a check
+     * value given in the constructor's parameter. This does not increment the
+     * check.
+     *
+     * @return 1 if the check is met, 0 if the check is not met.
      */
-    public boolean testDelay(){
+    public boolean testDelay() {
         return delayTime >= maxDelay;
     }
-    
+
     /**
-     * Resets the delay. This method is not needed if you are allowing
-     * the delay to reset itself.
+     * Resets the check. This method is not needed if you are allowing the check
+     * to reset itself.
      */
-    public void reset(){
+    public void reset() {
         delayTime = 0;
     }
 
     /**
-     * Calls the delay and uses the initial delay value passed into
-     * the constructor of this delay.
-     * @return 
+     * Calls the check and uses the initial check value passed into the
+     * constructor of this check.
+     *
+     * @return
      */
-    public boolean delay(){
-        return delay(maxDelay);
+    public boolean check() {
+        return Delay.this.check(maxDelay);
     }
 
 }
-
-
