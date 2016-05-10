1651814bac1bd5e7ffe83d29e2e90339e279dddf
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-10 10:37:46 -0600

    Formatted file and renamed variables

--- src/planet/util/SurfaceThread.java
+++ src/planet/util/SurfaceThread.java
@@ -7,24 +7,26 @@ import planet.Planet;
 import planet.surface.PlanetSurface;
 
 /**
- * A surface can be broken up into sections where a SurfaceThread can modify
- * and control that section. 
+ * A surface can be broken up into sections where a SurfaceThread can modify and
+ * control that section.
+ *
  * @author Richard DeSilvey
  */
 public class SurfaceThread extends MThread {
 
     private float absLowestHeight;
-    private AtomicInteger previousLowestHeight;
-    private AtomicInteger decPartLowestHeight;
-    
+    private AtomicInteger lowestHeightIntPart;
+    private AtomicInteger lowestHeightDecPart;
+
     /**
      * Lower bounds are inclusive, upper bounds are exclusive
      */
     protected Boundaries bounds;
     private int curFrame;
-    
+
     /**
      * Constructs a new SurfaceThread.
+     *
      * @param delay The amount of time to delay each frame in milliseconds
      * @param bounds The surface boundaries
      * @param name The name of this thread
@@ -34,38 +36,36 @@ public class SurfaceThread extends MThread {
 
         this.bounds = bounds;
         curFrame = 0;
-        previousLowestHeight = new AtomicInteger(Integer.MAX_VALUE);
-        decPartLowestHeight = new AtomicInteger(Integer.MAX_VALUE);
+        lowestHeightIntPart = new AtomicInteger(Integer.MAX_VALUE);
+        lowestHeightDecPart = new AtomicInteger(Integer.MAX_VALUE);
         absLowestHeight = Integer.MAX_VALUE;
     }
 
-    
     /**
-     * Each time the thread posts an update this method is called following
-     * a post-update call to postUpdate()
+     * Each time the thread posts an update this method is called following a
+     * post-update call to postUpdate()
      */
-    public final void update(){
+    public final void update() {
 
-        PlanetSurface surface = (PlanetSurface)Planet.self().getSurface();
-        
-        boolean sw  = (curFrame % 2) == 0;
+        PlanetSurface surface = (PlanetSurface) Planet.self().getSurface();
+
+        boolean sw = (curFrame % 2) == 0;
         int m;
         int lowerYBound = bounds.getLowerYBound();
         int upperYBound = bounds.getUpperYBound();
         int lowerXBound = bounds.getLowerXBound();
         int upperXBound = bounds.getUpperXBound();
-        
-        
-        int ystart  = sw ? lowerYBound : (upperYBound - 1);
-        int yinc    = sw ? 1 : -1; 
+
+        int ystart = sw ? lowerYBound : (upperYBound - 1);
+        int yinc = sw ? 1 : -1;
 
         try {
             for (int b = 0; b < 2; b++) {
                 for (int y = ystart; (sw ? (y < upperYBound) : (y >= 0)); y += yinc) {
-                    
-                    m = ((b > 0) && (y % 2 == 0)) ?  1 
-                       :((b > 0) && (y % 2 != 0)  ? -1 : 0);
-                    
+
+                    m = ((b > 0) && (y % 2 == 0)) ? 1
+                            : ((b > 0) && (y % 2 != 0) ? -1 : 0);
+
                     for (int x = ((y % 2) + m) + lowerXBound; x < upperXBound; x += 2) {
                         surface.updateGeology(x, y);
                         surface.updateOceans(x, y);
@@ -75,35 +75,35 @@ public class SurfaceThread extends MThread {
             }
             surface.heatMantel();
         } catch (Exception e) {
-            Logger.getLogger(SurfaceThread.class.getName()).log(Level.SEVERE, 
-              "An exception occured when updating the surface: {0}", getName());
+            Logger.getLogger(SurfaceThread.class.getName()).log(Level.SEVERE,
+                    "An exception occured when updating the surface: {0}", getName());
             e.printStackTrace();
         }
         curFrame++;
-        
-        int intPart = (int)absLowestHeight;
-        int decPart = (int)((absLowestHeight - intPart) * 10);
-        
-        previousLowestHeight.set(intPart);
-        decPartLowestHeight.set(decPart);
-        
+
+        int intPart = (int) absLowestHeight;
+        int decPart = (int) ((absLowestHeight - intPart) * 10);
+
+        lowestHeightIntPart.set(intPart);
+        lowestHeightDecPart.set(decPart);
+
         absLowestHeight = Integer.MAX_VALUE;
     }
-    
+
     public float getPreviousLowestHeight() {
-        
-        float decPart = decPartLowestHeight.get() / 10f;
-        decPart = previousLowestHeight.get() + decPart;
-        
+
+        float decPart = lowestHeightDecPart.get() / 10f;
+        decPart = lowestHeightIntPart.get() + decPart;
+
         return decPart;
     }
-    
-    private void updateMinimumHeight(int x, int y, PlanetSurface surface){
+
+    private void updateMinimumHeight(int x, int y, PlanetSurface surface) {
         float cellHeight = surface.getCellAt(x, y).getHeightWithoutOceans();
-        
-        if (cellHeight < absLowestHeight){
+
+        if (cellHeight < absLowestHeight) {
             absLowestHeight = cellHeight;
         }
     }
-   
+
 }
