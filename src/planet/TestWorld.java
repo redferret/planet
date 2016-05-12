033039f0bbe3b05d907364939d895224ca675c64
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-12 14:06:54 -0600

    Formatted file

--- src/planet/TestWorld.java
+++ src/planet/TestWorld.java
@@ -1,34 +1,33 @@
-
-
 package planet;
-
 
 /**
  * This class is used for testing purposes.
+ *
  * @author Richard DeSilvey
  */
 public class TestWorld extends Planet {
 
-    private static final int CELL_SIZE_M, DEFAULT_SIZE, 
+    private static final int CELL_SIZE_M, DEFAULT_SIZE,
             PLANET_DELAY, THREAD_COUNT, AGE_STEP_DELAY;
-    
-    static{
+
+    static {
         CELL_SIZE_M = 6;
         THREAD_COUNT = 1;
         PLANET_DELAY = 1;
         AGE_STEP_DELAY = 250;
         DEFAULT_SIZE = 256;
     }
-    
+
     /**
      * Constructs a default test world with a size of 256
      */
-    public TestWorld(){
+    public TestWorld() {
         this(DEFAULT_SIZE);
     }
-    
+
     /**
      * Constructs a new test world for the simulation.
+     *
      * @param worldSize The size of the world squared.
      */
     public TestWorld(int worldSize) {
