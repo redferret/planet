0a58df6e978abb7eedf151edd9d4234945fe4470
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-25 15:22:12 -0600

    Added JavaDoc

--- src/planet/util/TaskFactory.java
+++ src/planet/util/TaskFactory.java
@@ -2,7 +2,10 @@
 package planet.util;
 
 /**
- *
+ * A TaskFactory is an object factory that produces Task objects. This
+ * allows for each SurfaceThread of have it's own instance of a Task because
+ * that Task may have it's own instance variables the need to be unique to
+ * each thread.
  * @author Richard DeSilvey
  */
 public interface TaskFactory {
