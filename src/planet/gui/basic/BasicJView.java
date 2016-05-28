93febc1809ee2bd136db30bfe29dff0d68669e66
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-28 13:09:55 -0600

    Removed line that adds water to the surface and changed heating constant

--- src/planet/gui/basic/BasicJView.java
+++ src/planet/gui/basic/BasicJView.java
@@ -52,10 +52,8 @@ public class BasicJView extends JFrame implements DisplayAdapter {
         
         PlanetSurface surface = (PlanetSurface) testWorld.getSurface();
         surface.addToSurface(Layer.BASALT, 10000);
-        surface.addWaterToAllCells(5000);
         Planet.self().setTimescale(Planet.TimeScale.Geological);
-        Geosphere.heatDistributionCount = 350;
-        Geosphere.thermalInc = 150;
+        Geosphere.heatDistributionCount = 300;
 
         testWorld.play();
     }
