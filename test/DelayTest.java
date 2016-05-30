3feb97b8171f73ffa3b63ba7577f7f77c50ca684
Richard DeSilvey <rdesilvey@gmail.com>
2016-05-30 12:31:26 -0600

    Added new unit test

---
+++ test/DelayTest.java
@@ -0,0 +1,44 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import planet.util.Delay;
+
+/**
+ * Tests the Delay class.
+ * @author Richard DeSilvey
+ */
+public class DelayTest {
+    
+    private static final int FRAMES_TO_SKIP = 3;
+    
+    @Test
+    public void noResetOnDelayTest(){
+        
+        final boolean reset = false;
+        Delay testDelay = new Delay(FRAMES_TO_SKIP, reset);
+        
+        boolean expectedReturns[] = {false, false, true, true, true};
+        
+        for (boolean b : expectedReturns){
+            assertTrue(b == testDelay.check());
+        }
+    }
+    
+    /**
+     * Tests a delay object that resets for every x number of frames.
+     */
+    @Test
+    public void resetDelayTest(){
+        Delay testDelay = new Delay(FRAMES_TO_SKIP);
+        
+        boolean expectedReturns[] = {false, false, true, false, false, true};
+        
+        for (boolean b : expectedReturns){
+            assertTrue(b == testDelay.check());
+        }
+        
+    }
+    
+}
