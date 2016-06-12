d7c6856470eb8ee271eb515cab588cef995f07d0
Richard DeSilvey <rdesilvey@gmail.com>
2016-06-12 11:15:45 -0600

    Removed Factory and made Task a simple Task

--- src/planet/surface/Atmosphere.java
+++ src/planet/surface/Atmosphere.java
@@ -8,6 +8,7 @@ import planet.util.Delay;
 import planet.util.Task;
 import planet.util.TaskFactory;
 import static planet.surface.Surface.rand;
+import planet.util.TaskAdapter;
 import planet.util.Tools;
 /**
  *
@@ -17,66 +18,43 @@ public class Atmosphere extends Hydrosphere {
 
     public Atmosphere(int worldSize, int surfaceDelay, int threadsDelay, int threadCount) {
         super(worldSize, surfaceDelay, threadsDelay, threadCount);
-        produceTasks(new EvaporateFactory());
+        addTaskToThreads(new FastEvaportateTask());
     }
     
-    private class EvaporateFactory implements TaskFactory {
-
+    private class FastEvaportateTask extends TaskAdapter {
         @Override
-        public Task buildTask() {
-            return new EvaporateTask();
+        public void perform(int x, int y) {
+            if (y != 0) {
+                PlanetCell cell = getCellAt(x, y);
+                float w = getGridWidth();
+                float h = w / 2;
+                float rate = 0;
+
+                rate = calcLatitudeRate(y, h, w);
+
+                float amount = 15 * rate;
+                amount = cell.addOceanMass(-amount);
+
+                int rx = rand.nextInt(getGridWidth());
+                int ry = rand.nextInt(getGridWidth());
+
+                amount /= 4f;
+                getCellAt(Tools.checkBounds(rx + 1, getGridWidth()), ry).addOceanMass(amount);
+                getCellAt(Tools.checkBounds(rx - 1, getGridWidth()), ry).addOceanMass(amount);
+
+                getCellAt(rx, Tools.checkBounds(ry + 1, getGridWidth())).addOceanMass(amount);
+                getCellAt(rx, Tools.checkBounds(ry - 1, getGridWidth())).addOceanMass(amount);
+            }
         }
-        
-        private class EvaporateTask implements Task {
 
-            private Delay delay;
-
-            public EvaporateTask() {
-                delay = new Delay(10);
+        private float calcLatitudeRate(int y, float h, float w) {
+            if (0 <= y && y < h) {
+                return y / h;
+            } else if (h <= y && y < w) {
+                return (w - y) / h;
+            } else {
+                return 0;
             }
-            
-            @Override
-            public void perform(int x, int y) {
-                if (y != 0){
-                    PlanetCell cell = getCellAt(x, y);
-                    float w = getGridWidth();
-                    float h = w / 2;
-                    float rate = 0;
-
-                    rate = calcLatitudeRate(y, h, w);
-                    
-                    float amount = 15 * rate;
-                    amount = cell.addOceanMass(-amount);
-                    
-                    int rx = rand.nextInt(getGridWidth());
-                    int ry = rand.nextInt(getGridWidth());
-                    
-                    amount /= 4f;
-                    getCellAt(Tools.checkBounds(rx+1, getGridWidth()), ry).addOceanMass(amount);
-                    getCellAt(Tools.checkBounds(rx-1, getGridWidth()), ry).addOceanMass(amount);
-                    
-                    getCellAt(rx, Tools.checkBounds(ry+1, getGridWidth())).addOceanMass(amount);
-                    getCellAt(rx, Tools.checkBounds(ry-1, getGridWidth())).addOceanMass(amount);
-                }
-            }
-
-            private float calcLatitudeRate(int y, float h, float w) {
-                if (0 <= y && y < h){
-                    return y / h;
-                }else if (h <= y && y < w){
-                    return (w - y) / h;
-                }else{
-                    return 0;
-                }
-            }
-
-            @Override
-            public boolean check() {
-                return delay.check() && !PlanetSurface.suppressAtmosphere;
-            }
-            
         }
-        
     }
-    
 }
