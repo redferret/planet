895aac03341b8e9cf1b772bc8535354641ed587b
Richard DeSilvey <rdesilvey@gmail.com>
2016-06-12 22:00:19 -0600

    Changed constant

--- src/planet/cells/HydroCell.java
+++ src/planet/cells/HydroCell.java
@@ -189,7 +189,7 @@ public class HydroCell extends GeoCell {
         
         evapScale = 2.5f;
         sedimentCapacity = 0.50f;
-        minAngle = 0.02f;
+        minAngle = 1.5f;
         crossSectionalArea = Planet.self().getCellArea();
     }
     
