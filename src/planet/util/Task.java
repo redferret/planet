1a40f51731d030389ee208b5ccfdca22a353cdf1
Richard DeSilvey <rdesilvey@gmail.com>
2016-06-14 13:18:36 -0600

    Updated JavaDoc

--- src/planet/util/Task.java
+++ src/planet/util/Task.java
@@ -9,7 +9,8 @@ package planet.util;
 public interface Task {
     /**
      * This method will be called for each cell on the surface. This method
-     * will only be called if <code>check()</code> returns true.
+     * will only be called if <code>check()</code> returns true. Both x and y
+     * are bounded by [0, w) where w is the width of the map.
      * @param x The x coordinate of the cell.
      * @param y The y coordinate of the cell.
      */
