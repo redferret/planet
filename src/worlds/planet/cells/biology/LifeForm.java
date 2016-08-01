99e8f857aa67c74f331ca32e7a4315a01c8e59e1
Richard DeSilvey <rdesilvey@gmail.com>
2016-07-31 21:29:22 -0600

    Removed calculation of energy to fix error

--- src/worlds/planet/cells/biology/LifeForm.java
+++ src/worlds/planet/cells/biology/LifeForm.java
@@ -12,13 +12,6 @@ public class LifeForm {
     public static enum LifeFormType {Plant, Animal, Robot};
     
     /**
-     * The weight of the animal in kilograms.
-     */
-    private int size;
-    
-    private int metabolicRate;
-    
-    /**
      * The amount of energy this life form has. When a life form eats, this
      * value goes up otherwise it decays over time based on the size of the 
      * animal.
@@ -31,12 +24,10 @@ public class LifeForm {
     
     private Genetics genes;
     
-    public LifeForm(int size, int energy, LifeFormType type, Color c){
-        this.size = size;
+    public LifeForm(int energy, LifeFormType type, Color c){
         this.color = c;
         this.type = type;
         this.energy = energy;
-        metabolicRate = 0;
         genes = new Genetics();
     }
     
@@ -44,17 +35,12 @@ public class LifeForm {
         return color;
     }
 
-    public int getCapacity() {
-        return size;
-    }
-
     public int getConsumptionRate() {
         return 0;
     }
     
     public void changeEnergy(int amt){
-        energy += amt;
-        energy = (energy > size) ? size : ((energy <= 0) ? 0 : energy);
+
     }
     
     public void setEnergy(int energy){
@@ -66,7 +52,7 @@ public class LifeForm {
     }
 
     public LifeForm copy(){
-        return new LifeForm(size, energy, type, color);
+        return new LifeForm(energy, type, color);
     }
     
 }
