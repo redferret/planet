39d859ad6994b56bcb767b831b9758428afa863b
rdesilvey <rdesilvey@172.16.17.194>
2016-09-21 14:22:11 -0600

    Java Docs

--- src/engine/util/task/Boundaries.java
+++ src/engine/util/task/Boundaries.java
@@ -3,22 +3,32 @@
 package engine.util.task;
 
 /**
- *
+ * Boundaries define the bounds for each SurfaceThread running.
+ * It contains an upper and lower bounds for both axis.
  * @author Richard DeSilvey
  */
 public final class Boundaries {
 
     private int lowerXBound, upperXBound, lowerYBound, upperYBound;
-
-    public Boundaries() {
-        this(0, 0, 0, 0);
-    }
     
+    /**
+     * Constructs boundaries where both axis have the same ranges.
+     * @param lowerBound The lower bounds
+     * @param upperBound The upper bounds
+     */
     public Boundaries(int lowerBound, int upperBound){
         this(lowerBound, upperBound, lowerBound, upperBound);
     }
     
     public Boundaries(int lowerXBound, int upperXBound, int lowerYBound, int upperYBound) {
+    	
+    	if (lowerXBound < 0 || upperXBound < 0){
+    		throw new IllegalArgumentException("Can't have negative bounds");
+    	}
+    	if (lowerYBound < 0 || upperYBound < 0){
+    		throw new IllegalArgumentException("Can't have negative bounds");
+    	}
+    	
         this.lowerXBound = lowerXBound;
         this.upperXBound = upperXBound;
         this.lowerYBound = lowerYBound;
