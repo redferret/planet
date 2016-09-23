045073fae826669f7cd168f662ec087072ebb512
Richard DeSilvey <rdesilvey@gmail.com>
2016-09-23 12:51:57 -0600

    Moved task from geosphere

--- src/worlds/planet/surface/Atmosphere.java
+++ src/worlds/planet/surface/Atmosphere.java
@@ -3,13 +3,19 @@ package worlds.planet.surface;
 import worlds.planet.cells.atmosphere.Gas;
 import worlds.planet.cells.PlanetCell;
 import engine.util.Delay;
+import static engine.util.Tools.changeMass;
 import engine.util.task.Task;
 import engine.util.task.TaskFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import worlds.planet.cells.atmosphere.AtmoCell;
+import worlds.planet.cells.geology.GeoCell;
 import worlds.planet.enums.GasType;
+import worlds.planet.enums.Layer;
+import static worlds.planet.enums.SilicateContent.Mix;
+import static worlds.planet.enums.SilicateContent.Rich;
+import static worlds.planet.surface.Geosphere.windErosionConstant;
 
 /**
  *
@@ -32,6 +38,80 @@ public abstract class Atmosphere extends Hydrosphere {
         }
     }
 
+    private class AeolianFactory implements TaskFactory {
+
+        @Override
+        public Task buildTask() {
+            return new AeolianTask();
+        }
+
+        private class AeolianTask extends Task {
+
+            private Delay delay;
+
+            public void construct() {
+                delay = new Delay(150);
+            }
+
+            @Override
+            public void before() {
+            }
+
+            @Override
+            public void perform(int x, int y) {
+                PlanetCell cell = waitForCellAt(x, y);
+                aeolianErosion(cell);
+                release(cell);
+            }
+
+            public void aeolianErosion(GeoCell spreadFrom) {
+                convertTopLayer(spreadFrom);
+            }
+
+            public void convertTopLayer(GeoCell spreadFrom) {
+
+                float rockMass, sandMass;
+
+                if (spreadFrom.peekTopStratum() == null) {
+                    return;
+                }
+
+                Layer rockType = spreadFrom.peekTopStratum().getLayer();
+
+                GeoCell.SedimentBuffer eb = spreadFrom.getSedimentBuffer();
+                // Wind erosion
+                if (eb.getSediments() == 0 && !spreadFrom.hasOcean()
+                        && spreadFrom.getMoltenRockLayer().getMoltenRockFromSurface() < 1000) {
+
+                    Layer sedimentType;
+                    if (rockType.getSilicates() == Rich) {
+                        sedimentType = Layer.FELSIC;
+                    } else if (rockType.getSilicates() == Mix) {
+                        sedimentType = Layer.MIX;
+                    } else {
+                        sedimentType = Layer.MAFIC;
+                    }
+
+                    rockMass = spreadFrom.erode(windErosionConstant);
+                    sandMass = changeMass(rockMass, rockType, sedimentType);
+
+                    eb.transferSediment(sedimentType, sandMass);
+                }
+            }
+
+            @Override
+            public boolean check() {
+                return true;
+            }
+
+            @Override
+            public void after() {
+            }
+
+        }
+
+    }
+    
     private class SimpleClimateFactory implements TaskFactory {
         
         @Override
