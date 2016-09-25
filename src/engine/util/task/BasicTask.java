ab69bd9a2c1463a81f3c0e94186c58230a17e83e
Richard DeSilvey <rdesilvey@gmail.com>
2016-09-24 21:56:01 -0600

    Formatting

--- src/engine/util/task/BasicTask.java
+++ src/engine/util/task/BasicTask.java
@@ -1,20 +1,20 @@
-
-
 package engine.util.task;
 
 /**
  * A BasicTask is a task performed without updating all the cells on a map and
  * is similar to the TaskAdapter except the <code>check()</code> always returns
- * false and calls the new method <code>perform()</code>. Also the <code>construct()</code>
- * method doesn't need to be implemented but can be overridden.
+ * false and calls the new method <code>perform()</code>. Also the
+ * <code>construct()</code> method doesn't need to be implemented but can be
+ * overridden.
+ *
  * @author Richard DeSilvey
  */
 public abstract class BasicTask extends Task {
 
-	public void construct(){
-		// Basic Tasks don't need to be constructed
-	};
-	
+    public void construct() {
+        // Basic Tasks don't need to be constructed
+    }
+
     @Override
     public final void perform(int x, int y) {
     }
@@ -33,5 +33,4 @@ public abstract class BasicTask extends Task {
         return false;
     }
 
-    
 }
