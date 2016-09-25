bbc5e0fe9e80741fed552d239d443a1ad4c14ded
Richard DeSilvey <rdesilvey@gmail.com>
2016-09-25 10:29:18 -0600

    Edits to naming and comment

--- src/worlds/planet/geosphere/PlateTectonicsTask.java
+++ src/worlds/planet/geosphere/PlateTectonicsTask.java
@@ -23,12 +23,12 @@ public abstract class PlateTectonicsTask extends BasicTask {
     }
     
     /**
-     * Used to build a new plate using the given cell as the center of
+     * Used to build a new plate using the given cell position as the center of
      * the plate.
-     * @param centralCell The location of the center of the plate
+     * @param centralCell The center of the plate
      * @return The list of positions of each cell for the new plate.
      */
-    public List<Point> selectCellsFrom(Point centralCell){
+    public List<Point> buildPlate(Point centralCell){
         return null;
     }
 }
