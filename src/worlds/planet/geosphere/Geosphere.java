d0bd748c61e26e8d1c1a67b4bdd7861a0edcdc8a
rdesilvey <rdesilvey@172.16.17.194>
2016-09-28 09:59:16 -0600

    Modularized code for metamorphic and melting rock

--- src/worlds/planet/geosphere/Geosphere.java
+++ src/worlds/planet/geosphere/Geosphere.java
@@ -15,7 +15,7 @@ import worlds.planet.enums.RockType;
 import worlds.planet.geosphere.tasks.RockFormationTask;
 import worlds.planet.geosphere.tasks.PlateTectonicsTask;
 import worlds.planet.geosphere.tasks.GeologicalUpdateTask;
-
+import worlds.planet.geosphere.tasks.TransformationTask;
 import engine.util.Delay;
 import engine.util.Point;
 import engine.util.task.Task;
@@ -24,7 +24,6 @@ import engine.util.Tools;
 import engine.util.task.BasicTask;
 import engine.util.task.Boundaries;
 import engine.util.task.CompoundTask;
-
 import static engine.util.Tools.calcDepth;
 import static engine.util.Tools.calcMass;
 import static engine.util.Tools.checkBounds;
@@ -56,8 +55,7 @@ public abstract class Geosphere extends Surface {
         produceTasks(new GeologicalUpdateFactory());
         produceTasks(new SedimentationFactory());
         produceTasks(new PlateTectonicsFactory());
-//        produceTasks(new MetamorphicAndMeltingFactory());
-//        produceTasks(new HeatMantelFactory());
+        produceTasks(new TransformationFactory());
     }
 
     public long getAgeStamp() {
@@ -132,32 +130,14 @@ public abstract class Geosphere extends Surface {
         return points;
     }
     
-    public static int MELTING_PRESSURE;
-    public static int SEDIMENTARY_TO_METAMORPHIC;
-    public static int IGNEOUS_TO_METAMORPHIC;
-    public static int SLATE_TO_PHYLITE;
-    public static int PHYLITE_TO_SCHIST;
-    public static int SCHIST_TO_GNEISS;
-    public static float MASS_TO_MELT;
-
-    static {
-        MELTING_PRESSURE = 850;
-        SEDIMENTARY_TO_METAMORPHIC = 750;
-        IGNEOUS_TO_METAMORPHIC = 820;
-        SLATE_TO_PHYLITE = 780;
-        PHYLITE_TO_SCHIST = 800;
-        SCHIST_TO_GNEISS = 820;
-        MASS_TO_MELT = 2500;
-    }
-
-    private class ChangeRockFactory implements TaskFactory {
+    private class TransformationFactory implements TaskFactory {
 
         @Override
         public Task buildResource() {
-            return new ChangeRock();
+            return new TransformationResource();
         }
 
-        private class ChangeRock extends CompoundTask {
+        private class TransformationResource extends TransformationTask {
 
             public void setup() {
                 addSubTask(new MeltSubTask());
@@ -191,26 +171,11 @@ public abstract class Geosphere extends Surface {
                 @Override
                 public void after(){}
 
-                private void melt(GeoCell cell, float maxDepth) {
-
-                    float height;
-                    if (cell.peekBottomStratum() == null) {
-                        return;
-                    }
-                    height = cell.getHeight();
-
-                    if (height > maxDepth) {
-                        float diff = Tools.calcMass(height - maxDepth, 
-                                instance().getCellArea(), cell.getDensity());
-                        cell.remove(diff, false, false);
-                    }
-                }
             }
 
             private class MetaRockSubTask extends Task {
 
                 private Delay metaDelay;
-                private Stratum metaStratum;
 
                 public void construct() {
                     metaDelay = new Delay(10);
@@ -228,43 +193,7 @@ public abstract class Geosphere extends Surface {
                 @Override
                 public void perform(int x, int y) {
                     PlanetCell cell = waitForCellAt(x, y);
-                    List<Stratum> metamorphicStrata = new LinkedList<>();
-                    boolean metamorphicRockCanForm = true;
-                    Layer metaType = null, prevType;
-
-                    while (metamorphicRockCanForm) {
-                        float density = cell.getDensity();
-                        float cellDepth = cell.getHeight();
-                        float pressure = Tools.calcPressure(density, 9.8f, cellDepth);
-
-                        Stratum bottom = cell.peekBottomStratum();
-                        if (bottom == null) {
-                            return;
-                        }
-
-                        Layer bottomType = cell.peekBottomStratum().getLayer();
-                        prevType = metaType;
-                        metaType = getMetaLayer(bottomType, pressure);
-
-                        if (metaType != null) {
-                            metamorphisize(cell, metaType);
-                        } else {
-                            metamorphicRockCanForm = false;
-                        }
-
-                        if (prevType != null && metaType != prevType) {
-                            metamorphicStrata.add(metaStratum.copy());
-                            metaStratum = null;
-                        }
-
-                    }
-
-                    for (int i = metamorphicStrata.size() - 1; i >= 0; i--) {
-                        Stratum toAdd = metamorphicStrata.get(i);
-                        Layer type = toAdd.getLayer();
-                        float mass = toAdd.getMass();
-                        cell.add(type, mass, false);
-                    }
+                    metamorphisize(cell);
                     release(cell);
                 }
 
@@ -272,57 +201,6 @@ public abstract class Geosphere extends Surface {
                 public void after() {
                 }
 
-                private void metamorphisize(GeoCell cell, Layer metaType) {
-                    float massToChange;
-
-                    if (cell.peekBottomStratum() == null) {
-                        return;
-                    }
-
-                    Stratum bottom = cell.peekBottomStratum();
-                    Layer bottomType = bottom.getLayer();
-
-                    massToChange = calcMass(MASS_TO_MELT, instance().getCellArea(), bottomType);
-                    massToChange = removeAndChangeMass(cell, massToChange, bottomType, metaType);
-                    if (metaStratum == null) {
-                        metaStratum = new Stratum(metaType, massToChange);
-                    } else {
-                        metaStratum.addToMass(massToChange);
-                    }
-                }
-
-                private Layer getMetaLayer(Layer bottomType, float pressure) {
-                    Layer metaType = null;
-                    if (bottomType.getRockType() == RockType.SEDIMENTARY
-                            && pressure >= SEDIMENTARY_TO_METAMORPHIC) {
-                        if (bottomType == Layer.FELSIC_SANDSTONE) {
-                            metaType = Layer.QUARTZITE;
-                        } else if (bottomType == Layer.LIMESTONE) {
-                            metaType = Layer.MARBLE;
-                        } else {
-                            metaType = Layer.SLATE;
-                        }
-                    } else if (bottomType.getRockType() == RockType.IGNEOUS
-                            && pressure >= IGNEOUS_TO_METAMORPHIC) {
-                        metaType = Layer.GNEISS;
-                    } else {
-                        if ((bottomType == Layer.SLATE || bottomType == Layer.MARBLE
-                                || bottomType == Layer.QUARTZITE) && pressure >= SLATE_TO_PHYLITE) {
-                            metaType = Layer.PHYLITE;
-                        } else if (bottomType == Layer.PHYLITE && pressure >= PHYLITE_TO_SCHIST) {
-                            metaType = Layer.SCHIST;
-                        } else if (bottomType == Layer.SCHIST && pressure >= SCHIST_TO_GNEISS) {
-                            metaType = Layer.GNEISS;
-                        }
-                    }
-                    return metaType;
-                }
-
-                private float removeAndChangeMass(GeoCell cell, float mass, Layer bottomType, Layer toType) {
-                    float massToChange = cell.remove(mass, false, false);
-                    massToChange = Tools.changeMass(massToChange, bottomType, toType);
-                    return massToChange;
-                }
             }
         }
     }
