9adb2e9b5f59371ec02e71c7f3284356d2ccfa82
rdesilvey <rdesilvey@192.168.1.104>
2016-10-02 11:40:11 -0600

    Updated Java doc

--- src/engine/util/task/TaskAdapter.java
+++ src/engine/util/task/TaskAdapter.java
@@ -4,7 +4,7 @@ package engine.util.task;
 
 /**
  * TaskAdapter will have the check() method always return true. The check()
- * method can't be overridden.
+ * method can't be overridden. Additionally there is no construct() method.
  * @author Richard DeSilvey
  */
 public abstract class TaskAdapter extends Task {
